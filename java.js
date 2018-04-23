38933d544650c82b53dc6bb427515534e66655f4
indra910 <38657104+indra910@users.noreply.github.com>
2018-04-23 23:18:04 +0800

    Update java.js

--- java.js
+++ java.js
@@ -18,7 +18,14 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://succesonlife.blogspot.com/p/safelink.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);
+var x=Math.floor((Math.random()*5)+ 1);
+var xxx=null;
+if(x=="1"){xxx="/2018/04/essay-on-success.html"}
+if(x=="2"){xxx="/2018/04/hard-work-is-key-to-success.html"}
+if(x=="3"){xxx="/2018/04/speech-on-success.html"}
+if(x=="4"){xxx="/2018/04/5-real-life-tips-for-success-by-richard.html"}
+if(x=="5"){xxx="/2017/08/arctic-melt-season-changes-and-arctic.html"}a_to_ve[i].href="http://succesonlife.blogspot.com/p/safelink.html"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
